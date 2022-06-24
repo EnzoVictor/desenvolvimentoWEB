f02374b035df1a3531358202fd16fd4aa7c09db3
enzo.santana <enzo.santana@upx.com.br>
2022-06-24 16:38:02 -0300

    incluindo JavaScript

--- projetos/javascript_app_mosquito/js/javascript.js
+++ projetos/javascript_app_mosquito/js/javascript.js
@@ -6,3 +6,25 @@ function alturaLargura(){
     altura = innerHeight
     console.log(altura, largura)
 }
+
+alturaLargura()
+
+function posicaoMosca(){
+    var posicaoX = Math.floor(Math.random() * altura) - 90
+    var posicaoY = Math.floor(Math.random() * largura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //Criar um elemento HTML
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosca1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
