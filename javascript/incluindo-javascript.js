5bb9be0a3130d1d71660da5c39f810ed724f757b
enzo.santana <enzo.santana@upx.com.br>
2022-06-24 15:26:20 -0300

    incluindo JavaScript

---
+++ javascript/incluindo-javascript.js
@@ -0,0 +1,7 @@
+alert('Olá, seja bem vindo ao curso!!')
+
+/*
+    Selecionar um elemnto no DOM
+    Atualizar o valor do elemento em uma string
+*/
+document.getElementById('Nome').value = 'Oi'
---
+++ projetos/javascript_app_mosquito/js/javascript.js
@@ -0,0 +1,8 @@
+var altura = 0
+var largura = 0
+
+function alturaLargura(){
+    largura = innerWidth
+    altura = innerHeight
+    console.log(altura, largura)
+}
