8c26052515a9af73c6f174ee5e439bf4d596a3fb
enzo.santana <enzo.santana@upx.com.br>
2022-07-22 17:29:19 -0300

    orçamento pessoal FINALIZADO

---
+++ php/blocos_php.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <tittle>Curso PHP</tittle>
+  </head>
+  <body>
+
+    <?php
+        echo 'utilizando a tag padrão';
+    ?>
+
+    <br>
+
+    <?=  'utilizando a tag de impressão' ?>
+
+    <br>
+
+    <? 'Utilizando tag curta' ?>
+
+  </body>
+</html>
